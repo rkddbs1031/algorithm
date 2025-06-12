306f62a010403e46d1ecf38840abcd96dd99b241
Kangyoon Lee <rkddbs1031@gmail.com>
2025-06-12 17:34:04 +0900

    Add: [저자출제] 깊이 우선 탐색 순회

---
+++ 프로그래머스/코딩테스트_합격자_되기_자바스크립트편/38.깊이_우선_탐색_순회/깊이_우선_탐색_순회.js
@@ -0,0 +1,37 @@
+/*
+가중치가 x
+graph = [출발노드, 도착노드][]
+start = 시작노드
+*/
+function solution(graph, start) {
+  // stack
+
+  const graphObj = {};
+  graph.forEach(([u, v]) => {
+    if (!graphObj[u]) graphObj[u] = [];
+    graphObj[u].push(v);
+  });
+  // {
+  //   A: ['B', 'C'],
+  //   B: ['D', 'E'],
+  //   C: ['F'],
+  //   E: ['F']
+  // }
+
+  const visited = new Set();
+  const result = [];
+
+  dfs(start, visited, result, graphObj);
+  console.log(result);
+}
+
+function dfs(node, visited, result, graphObj) {
+  visited.add(node);
+  result.push(node);
+  (graphObj[node] || []).forEach((neighbor) => {
+    //인접한 노드가 방문하지 않았더라면 재귀
+    if (!visited.has(neighbor)) {
+      dfs(neighbor, visited, result, graphObj);
+    }
+  });
+}
