a2caba6eb6408099834a3144297ae29a8009b4bb
Kangyoon Lee <rkddbs1031@gmail.com>
2025-06-19 16:33:49 +0900

    Docs: 피로도 주석 처리

--- 프로그래머스/코딩테스트_합격자_되기_자바스크립트편/lv2/49.피로도/피로도.js
+++ 프로그래머스/코딩테스트_합격자_되기_자바스크립트편/lv2/49.피로도/피로도.js
@@ -1,3 +1,13 @@
+/*
+[[최소 피로도, 사용될 피로도]...]
+0번부터 굳이 돌지 않아도 됨.
+그렇다면 for문으로 인덱스를 중첩으로 돌 수 있게 해야함.
+이미 방문한 곳인지도 체크해야함.
+visited 사용해야함
+백 트래킹을 사용하려면  visited 다시 되돌려야함!!
+
+*/
+
 function solution(k, dungeons) {
   const visited = Array(dungeons.length).fill(false);
   let answer = 0;
