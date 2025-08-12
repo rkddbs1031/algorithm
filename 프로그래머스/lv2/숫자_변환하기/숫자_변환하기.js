84746072f8f7a6d8b8fe8f5c366d8a527c64e3c2
Kangyoon Lee <rkddbs1031@gmail.com>
2025-08-12 17:46:30 +0900

    FIX: [프로그래머스 level2] 숫자 변환하기 bfs 활용해서 다시 풀기

--- 프로그래머스/lv2/숫자_변환하기/숫자_변환하기.js
+++ 프로그래머스/lv2/숫자_변환하기/숫자_변환하기.js
@@ -6,6 +6,7 @@
  * - 최소 값을 구해야하기 때문에 Math.min() 활용
  */
 
+//dp
 function solution(x, y, n) {
   const dp = Array(y + 1).fill(Infinity); //인덱스: 만들고자 하는 수, 값: x에서 y까지 가는 데 필요한 최소 연산 횟수
   dp[x] = 0;
@@ -17,4 +18,45 @@ function solution(x, y, n) {
   }
 
   return dp[y] !== Infinity ? dp[y] : -1;
+}
+
+// bfs
+class Queue {
+  items = [];
+  front = 0;
+  rear = 0;
+
+  push(item) {
+    this.items.push(item);
+    this.rear++;
+  }
+
+  pop() {
+    return this.items[this.front++];
+  }
+
+  isEmpty() {
+    return this.front === this.rear;
+  }
+}
+
+function solution2(x, y, n) {
+  const queue = new Queue();
+  queue.push([x, 0]); // [현재 숫자, 연산 횟수]
+  const visited = new Set([x]);
+
+  while (!queue.isEmpty()) {
+    const [cur, calc] = queue.pop();
+    if (cur === y) return calc;
+
+    const candidates = [cur + n, cur * 2, cur * 3];
+    for (let c of candidates) {
+      if (c <= y && !visited.has(c)) {
+        visited.add(c);
+        queue.push([c, calc + 1]);
+      }
+    }
+  }
+
+  return -1;
 }
