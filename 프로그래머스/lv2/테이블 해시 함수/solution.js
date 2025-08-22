5ef1585e1563d6608d5d40ed875dc9ab6d749c78
Kangyoon Lee <rkddbs1031@gmail.com>
2025-08-22 17:38:53 +0900

    DOCS: [프로그래머스 level2] 테이블 해시 함수 문제풀이 작성

--- 프로그래머스/lv2/테이블 해시 함수/solution.js
+++ 프로그래머스/lv2/테이블 해시 함수/solution.js
@@ -1,3 +1,9 @@
+/**
+ * ## 문제 풀이
+ * 1. 주어진 데이터를 col-1번째 원소 기준으로 오름차순 정렬하고, 동일하다면 0번째 데이터를 기준으로 내림차순 정렬한다.
+ * 2. 정렬한 데이터를 기반으로 row_begin 행부터 row_end 행까지 데이터 튜플을 i로 나누고 모두 더한다.
+ * 3. 더한 값을 XOR 누적한다.
+ */
 function solution(data, col, row_begin, row_end) {
   data.sort((a, b) => {
     let A = a[col - 1];
@@ -14,5 +20,6 @@ function solution(data, col, row_begin, row_end) {
   for (let i = row_begin; i <= row_end; i++) {
     answer ^= data[i - 1].reduce((acc, cur) => acc + (cur % i), 0);
   }
+
   return answer;
 }
