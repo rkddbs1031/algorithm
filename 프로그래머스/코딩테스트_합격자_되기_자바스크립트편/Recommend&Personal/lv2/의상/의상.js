2792ac12c2529588fdf2a813e03aee0c4b2c194a
Kangyoon Lee <rkddbs1031@gmail.com>
2026-02-02 16:46:49 +0900

    fix:의상 문제 해석 추가

--- 프로그래머스/코딩테스트_합격자_되기_자바스크립트편/Recommend&Personal/lv2/의상/의상.js
+++ 프로그래머스/코딩테스트_합격자_되기_자바스크립트편/Recommend&Personal/lv2/의상/의상.js
@@ -1,3 +1,17 @@
+/*
+해석!
+headgear: 3가지 (모자A, 모자B, 안입기)
+eyewear: 2가지 (안경C, 안입기)
+전체 조합 = 3 × 2 = 6가지
+
+1. 모자A + 안경C
+2. 모자A + (안입기)
+3. 모자B + 안경C
+4. 모자B + (안입기)
+5. (안입기) + 안경C
+6. (안입기) + (안입기) ← 이거 빼야함!
+*/
+
 function solution(clothes) {
   const clothCoutObj = {};
   for ([, type] of clothes) {
